49bf35e2a39b332e4942cef6d19f559c8796e1c3
chavavenkat <chavavenkatarajesh@gmail.com>
2024-07-21 10:06:43 +0530

    first commit

--- mysql.sh
+++ mysql.sh
@@ -11,7 +11,6 @@ N="\e[0m"
 echo "Please enter DB password:"
 read -s mysql_root_password
 
-
 VALIDATE(){
    if [ $1 -ne 0 ]
    then
@@ -43,11 +42,11 @@ VALIDATE $? "Starting MySQL Server"
 # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 # VALIDATE $? "Setting up root password"
 
-#Below code will be useful fo idempotent nature
-mysql -h db.chavavenkat.online -uroot -p${mysql_root_password} -e 'show databases' &>>LOGFILE
+#Below code will be useful for idempotent nature
+mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>LOGFILE
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
     VALIDATE $? "MySQL Root password Setup"
 else
     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
