689aff183b41e95c1c3fb2d1f14e256c1a8fb95c
chavavrajesh1 <chavavrajesh1@gmail.com>
2026-01-09 21:04:49 +0530

    hello script

--- mysql.sh
+++ mysql.sh
@@ -5,6 +5,16 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
+VALIDATE(){
+    if [ $1 -ne 0]
+    then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
 if [ $USERID -ne 0 ]
 then
     echo "Please run this script with root access."
